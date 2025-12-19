7222b5d7f9db607c690ebe353b7682759953e4aa
septvean <septvean@gmail.com>
2025-12-19 19:12:20 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE_DIR="/data/septvean/documents/public"
+
+cd $(dirname $0)
+
+[ ! -d ${SOURCE_DIR} ] && echo "source dir not exists" && exit 1
+
+[ -d ./docs ] && rm -rf ./docs
+
+mv -v ${SOURCE_DIR} ./docs
+
+git restore docs/CNAME
+
+# git add .
+
+# git commit -m "update"
